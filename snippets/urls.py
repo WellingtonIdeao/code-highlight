7fc2435531c287f84e748eaa425d481d14ddf66a
Wellington Ideão <wellington.ideao@gmail.com>
2022-06-29 21:18:35 -0300

    Added SnippetDetailView

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SnippetListView
+from .views import SnippetListView, SnippetDetailView
 app_name = 'snippets'
 
 urlpatterns = [
     path('snippets/', SnippetListView.as_view(), name='list'),
+    path('snippets/<int:pk>/', SnippetDetailView.as_view(), name='detail'),
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views import View
 from django.utils.decorators import method_decorator
@@ -29,4 +29,33 @@ class SnippetListView(View):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class SnippetDetailView(View):
+
+    def get(self, request, *args, **kwargs):
+        snippet = get_object_or_404(Snippet, pk=kwargs['pk'])
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    def put(self, request, *args, **kwargs):
+        snippet = get_object_or_404(Snippet, pk=kwargs['pk'])
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    def delete(self, request, *args, **kwargs):
+        snippet = get_object_or_404(Snippet, pk=kwargs['pk'])
+        snippet.delete()
+        return HttpResponse(status=204)
+
+
+
+
+
+
 
