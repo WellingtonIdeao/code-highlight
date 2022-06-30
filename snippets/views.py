40f8c20a885844595454eb2627c5e30ed4eddec7
Wellington Ideão <wellington.ideao@gmail.com>
2022-06-30 20:53:36 -0300

    Fix APIView

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,6 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -16,12 +17,12 @@ class SnippetListView(APIView):
     """
         List all code snippets, or create a new snippet.
     """
-    def get(self, request, *args, **kwargs):
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -37,35 +38,25 @@ class SnippetDetailView(APIView):
     def get_object(self, pk=None):
         if pk is None:
             pk = self.kwargs.get('pk')
-        obj = Snippet.objects.get(pk=pk)
-        return obj
-
-    def get(self, request, *args, **kwargs):
         try:
-            snippet = self.get_object()
+            return Snippet.objects.get(pk=pk)
         except Snippet.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            raise Http404
 
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    def put(self, request, *args, **kwargs):
-        try:
-            snippet = self.get_object()
-        except Snippet.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, *args, **kwargs):
-        try:
-            snippet = self.get_object()
-        except Snippet.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        snippet = self.get_object()
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
