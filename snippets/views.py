8803a15659c8342c85db107072ab5502e714537e
Wellington Ideão <wellington.ideao@gmail.com>
2022-06-30 21:24:55 -0300

    Added generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -17,38 +17,18 @@ from .serializers import SnippetSerializer
 # Create your views here.
 
 @method_decorator(csrf_exempt, name='dispatch')
-class SnippetListView(mixins.ListModelMixin,
-                      mixins.CreateModelMixin,
-                      generics.GenericAPIView):
+class SnippetListView(generics.ListCreateAPIView):
     """
         List all code snippets, or create a new snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
 
 @method_decorator(csrf_exempt, name='dispatch')
-class SnippetDetailView(mixins.RetrieveModelMixin,
-                        mixins.UpdateModelMixin,
-                        mixins.DestroyModelMixin,
-                        generics.GenericAPIView):
+class SnippetDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
         Retrieve, update or delete a code snippet.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
