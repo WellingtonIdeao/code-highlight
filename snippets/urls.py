7f13d21eceb54a8956cab5f0378069ef52175553
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-02 11:46:45 -0300

    Added HyperLinkedModel serializer

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import SnippetListView, SnippetDetailView, UserListView, UserDetailView
+from .views import SnippetListView, SnippetDetailView, UserListView, UserDetailView, api_root,\
+    SnippetHighlightView
 from rest_framework.urlpatterns import format_suffix_patterns
 app_name = 'snippets'
 
 urlpatterns = [
+    path('', api_root),
     path('snippets/', SnippetListView.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', SnippetDetailView.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', SnippetHighlightView.as_view(), name='snippet-highlight'),
     path('users/', UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+
 
 ]
 
--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
@@ -9,6 +13,16 @@ from .permissions import IsOwnerOrReadOnly
 
 
 # Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response(
+        {
+            'users': reverse('user-list', request=request, format=format),
+            'snippets': reverse('snippet-list', request=request, format=format)
+        }
+    )
+
 
 class SnippetListView(generics.ListCreateAPIView):
     """
@@ -47,3 +61,12 @@ class UserDetailView(generics.RetrieveAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlightView(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
