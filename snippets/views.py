85a400947a68530aa275405a4c6d0a05a68c7975
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-03 14:42:56 -0300

    Using DefaultRouter to design URL conf

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.models import User
-
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -15,15 +11,6 @@ from .permissions import IsOwnerOrReadOnly
 
 
 # Create your views here.
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response(
-        {
-            'users': reverse('user-list', request=request, format=format),
-            'snippets': reverse('snippet-list', request=request, format=format)
-        }
-    )
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
